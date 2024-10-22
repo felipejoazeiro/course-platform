d5f9823614ad65a982a1dc4c3861055a12f9cc67
felipe.joazeiro <felipe.joazeiro@ictcondor.com.br>
2024-10-22 16:20:18 -0300

    db initial structure

---
+++ db/tables.sql
@@ -0,0 +1,12 @@
+create table employee(
+	id smallserial primary key not null,
+	name varchar(50) not null,
+	registration varchar(50) not null
+);
+
+create table access(
+	id smallserial primary key not null,
+	username varchar(50) not null unique,
+	password varchar(50) not null,
+	fk_employee int not null
+);

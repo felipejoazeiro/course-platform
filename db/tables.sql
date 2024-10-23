8617407b741d6564ea3fe09ac493fc31d3a8cb4d
felipe.joazeiro <felipe.joazeiro@ictcondor.com.br>
2024-10-23 16:33:04 -0300

    Changes on register table

--- db/tables.sql
+++ db/tables.sql
@@ -1,7 +1,8 @@
 create table employee(
 	id smallserial primary key not null,
 	name varchar(50) not null,
-	registration varchar(50) not null
+	registration varchar(50) not null,
+	email varchar(50) not null
 );
 
 create table access(

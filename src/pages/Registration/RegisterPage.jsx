5a3425e296e48f676734f54cbf24b4cb24ccaa6d
felipe.joazeiro <felipe.joazeiro@ictcondor.com.br>
2024-10-25 10:37:59 -0300

    Tela de registro funcional

--- src/pages/Registration/RegisterPage.jsx
+++ src/pages/Registration/RegisterPage.jsx
@@ -1,34 +1,40 @@
 import React, {useState} from "react"
 import axios from 'axios';
-import { TextField, Button, Paper, Typography, Container, Snackbar, Alert } from '@mui/material';
+import { TextField, Button, Paper, Typography, Container, Snackbar, Alert, Link } from '@mui/material';
 import './styles.css'
+import { useNavigate } from "react-router-dom";
 
 function RegisterPage(){
     const [name, setName] = useState('');
-    const [register, setRegister] = useState('');
+    const [registration, setRegistration] = useState('');
     const [email, setEmail] = useState('')
     const [message, setMessage] = useState('')
     const [popup, setPopup] = useState(false)
     const [severity, setSeverity] = useState('error')
 
+    const navigate = useNavigate();
     const handleSubmit = async (e) =>{
         e.preventDefault();
-
-        const employee = {name, register, email};
+        const employee = {name, registration, email};
 
         try{
+            console.log(process.env.REACT_APP_API_URL)
+            console.log(employee)
             const response = await axios.post(`${process.env.REACT_APP_API_URL}/register`,employee)
+            
+            console.log(response.status);
             setMessage('Funcionário cadastrado com sucesso')
             setSeverity('success')
             setPopup(true)
 
             setName('')
-            setRegister('')
+            setRegistration('')
             setEmail('')
 
         }catch(error){
             if(error.response){
-                setMessage(error.response.data.message || 'Erro desconhecido')
+                console.log(error.response.data.detail.message)
+                setMessage(error.response.data.detail.message || 'Erro desconhecido')
             }else{
                 setMessage('Erro de rede')
             }
@@ -41,25 +47,36 @@ function RegisterPage(){
         setPopup(false)
     }
 
+    const goToLogin=()=>{
+        navigate("/")
+    }
+
     return (
-        <Container className = "container_register" component="main" maxWidth = "xs">
-            <Paper className="card_register" elevation={6}>
-                <Typography variant = "h5" align="center">
-                    Register
-                </Typography>
-                <form className="form_login">
-                    <TextField variant="outlined" fullWidth label="Nome" margin ="normal" value={name} onChange={(e)=> setName(e.target.value)}/>
-                    <TextField variant="outlined" fullWidth label="Matricula"  margin="normal" value={register}  onChange={(e)=>setRegister(e.target.value)}/>
-                    <TextField variant="outlined" fullWidth label="E-mail" type="email" margin="normal" value={email}  onChange={(e)=>setEmail(e.target.value)}/>
-                    <Button onSubmit={handleSubmit} className="button_login" type="submit" variant="contained" color="primary" fullWidth>Entrar</Button>
-                </form>
-            </Paper>
-            <Snackbar open={popup} autoHideDuration={6000} onClose = {handlePopup}>
-                <Alert onClose={handlePopup} severity={severity}>
-                    {message}
-                </Alert>
-            </Snackbar>
-        </Container>
+        <div>
+            <Container className = "container_register" component="main" maxWidth = "xs">
+                <Paper className="card_register" elevation={6}>
+                    <Typography variant = "h5" align="center">
+                        Registro
+                    </Typography>
+                    <form className="form_login">
+                        <TextField variant="outlined" fullWidth label="Nome" margin ="normal" value={name} onChange={(e)=> setName(e.target.value)}/>
+                        <TextField variant="outlined" fullWidth label="Matricula"  margin="normal" value={registration}  onChange={(e)=>setRegistration(e.target.value)}/>
+                        <TextField variant="outlined" fullWidth label="E-mail" type="email" margin="normal" value={email}  onChange={(e)=>setEmail(e.target.value)}/>
+                        <Button onSubmit={handleSubmit} onClick={handleSubmit} className="button_login" type="submit" variant="contained" color="primary" fullWidth>Registrar</Button>
+                    </form>
+                    <Typography variant="body2" align="right" style={{marginTop: 10}}>
+                        <Link onClick={goToLogin} href="#" style={{ color: 'blue' }}>
+                            Voltar para login
+                        </Link>
+                    </Typography>
+                </Paper>
+                <Snackbar open={popup} autoHideDuration={6000} onClose = {handlePopup}>
+                    <Alert onClose={handlePopup} severity={severity}>
+                        {message}
+                    </Alert>
+                </Snackbar>
+            </Container>
+        </div>
     )
 }
 
